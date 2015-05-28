d228319907d8bbed8ad12abcfaf6b1d719f4229c
sfnaila <naila0604@gmail.com>
2015-05-28 09:44:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+     <body style bgcolor="#CCCCCC"">
+     <center>
+     <div style="background-color:#C36;">
+     <h1 style="color:#FFF"> PROGRAM OLAH KATA </h1></center>
+     <br />
+     <br />
+     </div>
+	
+<div style="font-size:20px;">    
+<center>
+<?php
+	
+	echo "<form action='proses.php' method='post' />";
+	echo "Ketik Huruf: <input type='text' name='text' required placeholder='masukkan kata'/>";
+	echo " <input type='submit' name='acak' value='acak'/>";
+
+?>
+</center>
+</div>
+	</body>
+
+
+
+</html>
